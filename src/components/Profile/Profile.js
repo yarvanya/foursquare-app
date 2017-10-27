fd1abacbbe73c9774d4453dee8965bda4a9e9f3c
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-10-27 17:09:38 +0300

    Added styles for the Profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { GoogleLogout } from 'react-google-login';
+import './profile.css';
 
 class Profile extends React.Component {
   constructor(props) {
@@ -37,14 +38,16 @@ class Profile extends React.Component {
       <div>
         {!isLoggedIn &&
         <GoogleLogin
+          className="loginButton"
           clientId="482936635819-kfr4pl8qjvo7nd957ho6dn3et224jbjk.apps.googleusercontent.com"
           buttonText="Login with Google"
           onSuccess={this.signIn}
         />}
         {isLoggedIn &&
         <div>
-          <h1>{this.state.name}</h1>
+          <h1 className="profileGreeting">Welcome, {this.state.name}</h1>
           <GoogleLogout
+            className="logoutButton"
             buttonText="Logout"
             onLogoutSuccess={this.signOut}
           >
